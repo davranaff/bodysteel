43e0573793dac7d255e550d68f30d086cad38d16
Sukhrob Davronov <sukhrob.davronov@osg.uz>
2024-02-23 22:53:54 +0500

    models is ready on 70%

--- store/models.py
+++ store/models.py
@@ -10,11 +10,60 @@ def blog_directory_path(instance, filename):
     return 'blog/{0}/{1}'.format(instance.name, filename)
 
 
+def brand_directory_path(instance, filename):
+    return 'brand/{0}/{1}'.format(instance.name, filename)
+
+
 class BaseModel(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         abstract = True
+
+
+class Menu(BaseModel):
+    name = models.CharField(max_length=100, unique=True, verbose_name='Дайте название для этого меню')
+
+    about = RichTextField(verbose_name='О нас', help_text='Текст для раздела о нас')
+    blog = RichTextField(verbose_name='Блог', help_text='Текст для раздела Блог')
+    set_product = RichTextField(verbose_name='Комплект', help_text='Текст для раздела Комплект')
+    delivery_and_payment = RichTextField(verbose_name='Доставка и Оплата',
+                                         help_text='Текст для раздела Доставка и Оплата')
+
+    is_active = models.BooleanField(default=True, verbose_name='Активировать',
+                                    help_text='Если отключено, то не будет видно', unique=True)
+
+    def __str__(self):
+        return self.about
+
+    class Meta:
+        verbose_name = 'Меню'
+        verbose_name_plural = 'Меню'
+
+
+class Filial(BaseModel):
+    name = models.CharField(max_length=100, verbose_name='Название филиала')
+    address = models.CharField(max_length=255, verbose_name='Адрес филиала (без ссылки)',
+                               help_text='Адрес филиала, а не ссылка (Рес. Узбекистан, г. Бухара, ул. Абдулла кодирий 100 дом)')
+
+    work_time_start = models.TimeField(verbose_name='Время старта работы', help_text='от ПН. до СБ.')
+    work_time_end = models.TimeField(verbose_name='Время заканчиваие работы', help_text='от ПН. до СБ.')
+
+    day_off = models.CharField(max_length=255, verbose_name='Выходной',
+                               help_text='Если вы работаете в выходные дни, то пишите время с какого часа до какого вы работаете, если нет то пишилте Выходной')
+
+    phone = models.CharField(max_length=13, verbose_name='Телефон номер филиала')
+
+    address_url = models.URLField(verbose_name='Адрес филиала (только ссылка)')
+
+    photo = models.ImageField(upload_to='filial/', verbose_name='Фотография филиала')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Филиал'
+        verbose_name_plural = 'Филиалы'
 
 
 class SetOfProducts(BaseModel):
@@ -61,3 +110,34 @@ class Blog(BaseModel):
         verbose_name_plural = "Блоги"
 
 
+class Brand(BaseModel):
+    name = models.CharField(max_length=100, verbose_name="Название бренда", unique=True)
+    photo = models.ImageField(upload_to=blog_directory_path, verbose_name="Фотография бренда")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Бренд"
+        verbose_name_plural = "Бренды"
+
+
+class Product(BaseModel):
+    name = models.CharField(max_length=100, verbose_name='Название Продукта', unique=True)
+    description = RichTextField(verbose_name='Описание Товара')
+
+    price = models.PositiveBigIntegerField(verbose_name='Стоимость товара')
+
+    is_new = models.BooleanField(default=True, verbose_name='Новый товар',
+                                 help_text='Если вкл. то на сайте будет показывать, что этот товар "Новинка"')
+
+    quantity = models.PositiveIntegerField(default=0, verbose_name='Кол-во. Товара на складе')
+
+    discounted_price = models.PositiveBigIntegerField(default=0, verbose_name='Скидочная цена',
+                                                      help_text='Если есть скидка на товар, то вы '
+                                                                'должны здесь написать на какую сумму '
+                                                                'скидка (пример: сам товар стоит 120000 сум, '
+                                                                'скидка на 20000 сум. итог:100000'
+                                                                'здесь 20000 сум скидочная цена)')
+
+    slug = models.SlugField(max_length=255, unique=True)
