a6bb7e6f713889f83fa0cc3ef913c5c48affc992
intel <deff0427@gmail.com>
2024-03-13 04:44:48 +0500

    add static and media roots in config/urls.py

--- config/urls.py
+++ config/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import permissions
@@ -26,3 +28,6 @@ urlpatterns = [
         path('users/', include(('users.urls', 'users'), namespace='users')),
     ]))
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
