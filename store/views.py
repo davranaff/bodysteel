7abdc1447041384a06e19da24d9cfa975fc23034
intel <deff0427@gmail.com>
2024-03-13 06:11:07 +0500

    cheange store/views.py

--- store/views.py
+++ store/views.py
@@ -25,12 +25,12 @@ class HomaPageAPIView(APIView):
                                                                       many=True).data
         serializer_category = CategorySerializer(Category.objects.all(), many=True).data
         serializer_leader_products = ProductSerializer(
-            Product.objects.with_rating().filter(baskets__order__isnull=False)[:5],
+            Product.objects.with_rating().with_favorite(request.auth).filter(baskets__order__isnull=False)[:5],
             many=True).data
         serializer_sale_products = ProductSerializer(
-            Product.objects.with_rating().filter(discounted_price__gt=0)[:5], many=True).data
+            Product.objects.with_rating().with_favorite(request.auth).filter(discounted_price__gt=0)[:5], many=True).data
         serializer_latest_products = ProductSerializer(
-            Product.objects.with_rating().all().order_by('-created_at')[:8],
+            Product.objects.with_rating().with_favorite(request.auth).all().order_by('-created_at')[:8],
             many=True).data
         serializer_brands = BrandSerializer(Brand.objects.all()[:6], many=True).data
         serializer_blogs = BlogSerializer(Blog.objects.all()[:6], many=True).data
@@ -95,7 +95,7 @@ class SetOfProductViewSet(viewsets.ViewSet):
         return Response({'data': {'set_of_products': serializer, **menu_serializer}}, status=status.HTTP_200_OK)
 
     def retrieve(self, request, pk):
-        set_of_product = Product.objects.with_favorite_flag(request.auth).with_all_rating().filter(
+        set_of_product = Product.objects.with_rating().with_favorite(request.auth).filter(
             set_of_products_id=pk)
         serializer = ProductSerializer(set_of_product, many=True).data
 
@@ -150,7 +150,7 @@ class ProductViewSet(viewsets.ViewSet):
         return Response({'data': serializer}, status=status.HTTP_200_OK)
 
     def retrieve(self, request, pk):
-        product = get_object_or_404(Product.objects.with_favorite_flag(request.auth).with_all_rating(), pk=pk)
+        product = get_object_or_404(Product.objects.with_favorite(request.auth).with_rating(), pk=pk)
         serializer = ProductSerializer(product, many=False).data
 
         return Response({'data': serializer})
@@ -165,7 +165,7 @@ class CategoryViewSet(viewsets.ViewSet):
         return Response({'data': serializer}, status=status.HTTP_200_OK)
 
     def retrieve(self, request, pk):
-        category_products = Product.objects.with_favorite_flag(request).with_all_rating().filter(category_id=pk)
+        category_products = Product.objects.with_favorite(request.auth).with_rating().filter(category_id=pk)
         serializer = ProductSerializer(category_products, many=True).data
 
         return Response({'data': serializer}, status=status.HTTP_200_OK)
