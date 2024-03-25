b9b674f313f408efa2ed20038164733126b69f10
user <deff0427@gmail.com>
2024-03-25 12:17:58 +0500

    fixed cors error v0.1

--- config/settings.py
+++ config/settings.py
@@ -6,7 +6,7 @@ SECRET_KEY = 'django-insecure-i(!^3@q&)btg5%skz2wc0tr(t(3avq20p3+*alul)$wv2(awq(
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://bodysteel.vercel.app', "http://localhost:8000", "http://localhost:3000",]
 
 INSTALLED_APPS = [
     'ckeditor',
@@ -55,6 +55,7 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:8000",
     "http://localhost:3000",
+    "https://bodysteel.vercel.app"
 ]
 
 ROOT_URLCONF = 'config.urls'
