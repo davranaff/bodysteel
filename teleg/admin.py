7f827b8a441fd7be703bf7b7d91ee842ea61e0c2
Davranaff <davranaff@Noutbuk-Davranaff.local>
2025-04-13 18:25:15 +0500

    fix chat

--- teleg/admin.py
+++ teleg/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from teleg.models import SecretPhrase
+from teleg.models import SecretPhrase, Chat
 
 # Register your models here.
 
@@ -9,3 +9,9 @@ class OrdersAdmin(admin.ModelAdmin):
     empty_value_display = "-пусто-"
     list_display = ['id', 'phrase', 'expired_date',]
     list_editable = ['phrase', 'expired_date',]
+
+@admin.register(Chat)
+class ChatAdmin(admin.ModelAdmin):
+    empty_value_display = "-пусто-"
+    list_display = ['id', 'chat_id', 'first_name', 'last_name', 'username',]
+    list_editable = ['first_name', 'last_name', 'username',]
